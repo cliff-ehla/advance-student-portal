ef173e3160c1e13458f7cf1b3172cf3f5f6cf75f
cliff <cliff.wong@ehlacademy.org>
2022-01-13 15:57:26 +0800

    fail to fetch dynamic error: trigger refresh

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -13,6 +13,14 @@
 <script>
 	export let error_message;
 	export let status;
+	import {sentry} from "$lib/sentry";
+
+	if (error_message.includes('Failed to fetch dynamically imported module')) {
+		window.location.reload()
+		sentry.log('Trigger a full refresh from "Failed to fetch dynamically imported module"')
+	} else {
+		sentry.log(error_message)
+	}
 </script>
 
 <div class="container text-center py-12">
