0f3313ef75add4c7c1955a48d3072255660f0d3e
cliff <cliff.wong@ehlacademy.org>
2022-02-24 14:57:02 +0800

    scroll to date

--- src/routes/schedule/[yyyy]-[mm]-[dd]/list.svelte
+++ src/routes/schedule/[yyyy]-[mm]-[dd]/list.svelte
@@ -24,6 +24,7 @@
 	import isToday from "dayjs/plugin/isToday.js";
 	import isBetween from "dayjs/plugin/isBetween.js";
 	import ZoomPreview from '$lib/zoom/zoom-preview.svelte'
+	import * as animateScroll from "svelte-scrollto";
 
 	export let zoom_list
 	dayjs.extend(isToday)
@@ -50,11 +51,25 @@
 		return results
 	}
 
+	const scrollToDate = () => {
+		animateScroll.scrollTo({
+			element: `[data-date="${date_key}"]`,
+			offset: -48,
+			onDone: (el) => {
+				if (!el) return console.log('no element')
+				el.classList.add('bg-yellow-500')
+				setTimeout(() => {
+					el.classList.remove('bg-yellow-500')
+				}, 2000)
+			}
+		})
+	}
+
 </script>
 
 <div class="max-w-screen-lg mx-auto">
 	{#each zoom_list_by_date as date}
-		<div data-date={dayjs(date.date).format('YYYY-MM-DD')} class="transition-colors duration-1000 flex items-start">
+		<div use:scrollToDate data-date={dayjs(date.date).format('YYYY-MM-DD')} class="transition-colors duration-1000 flex items-start">
 			<div class="w-40 px-2 flex items-center">
 				<div class:active={dayjs(date.date).isToday()} class="rounded-full w-10 h-10 cc mr-2">{dayjs(date.date).format('D')}</div>
 				<p class="text-xs uppercase mt-0.5">{dayjs(date.date).format('MMM, ddd')}</p>
