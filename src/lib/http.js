58adc1af7efb07b77ed95d7f6666c36b541753ba
cliff <cliff.wong@ehlacademy.org>
2022-03-24 10:22:49 +0800

    upgrade http

--- src/lib/http.js
+++ src/lib/http.js
@@ -1,39 +1,69 @@
 import {sentry} from "$lib/sentry";
+import {is_loading} from "$lib/store/is_loading";
+import {notifications} from "$lib/store/notification";
+import {browser} from "$app/env";
 
+const onRes = async (res, notification) => {
+	try {
+		let status = res.status
+		if (status >= 500)
+			throw status === 504 ? '504 timeout' : 'Internal server error'
+		const {success, data, metadata, debug} = await res.json()
+		if (!success && debug && debug.err_code === 401 && browser && !['/login', '/logout'].includes(window.location.pathname)) {
+			window.location.replace("/logout")
+		}
+		const actually_not_success = data ? data.status === 'failure' : false
+		is_loading.set(false)
+		if (!!notification) {
+			if (!success || actually_not_success) {
+				const message = actually_not_success ? data.debug_msg : debug.debug_msg
+				notifications.alert('Oops...' + message)
+			} else {
+				notifications.success(notification)
+			}
+		}
+		return {success, data, metadata, debug, status}
+	} catch (e) {
+		const debug_msg = `${res.url} ${e}`
+		notifications.alert(debug_msg)
+		sentry.log(e)
+		return {
+			success: false,
+			status: 500,
+			debug: {
+				debug_msg
+			}
+		}
+	}
+}
+
+// handle 到 usermodel 500, 或者自己 500
 const http = (() => {
-	async function get (fetch, resource, query) {
+	async function get (fetch, resource, query, config = {}) {
+		const {notification} = config
 		if (query) {
 			for (const property in query) {
-				// TODO ? &
-				resource += `?${property}=${query[property]}`
+				let char = resource.includes('?') ? '&' : '?'
+				resource += `${char}${property}=${query[property]}`
 			}
 		}
+		is_loading.set(true)
 		const res = await fetch('/api' + resource)
-		const {success, data, metadata, debug} = await res.json()
-		return {success, data, metadata, debug}
+		return onRes(res, notification)
 	}
 
-	async function post (fetch, resource, body = {}) {
-		// an empty object is necessary, otherwise result fatal error when not passing body params
-		try {
-			const res = await fetch('/api' + resource, {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json'
-				},
-				body: body && JSON.stringify(body)
-			})
-			const {success, data, metadata, debug} = await res.json()
-			return {success, data, metadata, debug}
-		} catch (e) {
-			console.log('fatal error: this mostly happened when usermodel do not return a json body', e)
-			return {
-				success: false,
-				data: false,
-				status: 400,
-				debug: 'Usermodel return fatal error'
-			}
-		}
+	// an empty object is necessary, otherwise result fatal error when not passing body params
+	async function post (fetch, resource, body = {}, config = {}) {
+		const {notification} = config
+		is_loading.set(true)
+		const res = await fetch('/api' + resource, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: body && JSON.stringify(body)
+		})
+		return onRes(res, notification)
 	}
 
 	return {
@@ -42,17 +72,16 @@ const http = (() => {
 	}
 })()
 
-const onFail = (debug) => {
-	if (debug.err_code === 401) {
+const onFail = (debug, status) => {
+	if ((debug.err_code) === 401) {
 		return {
 			status: 302,
-			redirect: '/login'
+			redirect: '/logout'
 		}
 	}
-	sentry.log(debug)
 	return {
-		error: new Error(debug.debug_msg),
-		status: 400
+		error: debug && debug.debug_msg,
+		status
 	}
 }
 
